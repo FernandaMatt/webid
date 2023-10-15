9cc393de480637206e192f55faf6ebdd63716305
psydenst <psydenst.dev@gmail.com>
2023-10-14 22:20:25 -0300

    menu-icon

--- webid_front/components/navbar.js
+++ webid_front/components/navbar.js
@@ -1,8 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { useWindowSize } from "@uidotdev/usehooks";
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from '../styles/Navbar.module.css';
+import menuIcon from '../public/menu-icon-white.png';
 
-export default function Navbar () {
+
+
+export default function Navbar ()
+{
+
+	const [menuOpen, setMenuOpen] = useState(false);
+	const size = useWindowSize();
+
+	const toggleMenu = () => {
+		setMenuOpen(!menuOpen);
+	}
+
+	const closeMenu = () => {
+		setMenuOpen(false);
+	}
+
 	return (
 		<nav className={styles.navbar}>
 			<Link href="/">
@@ -14,7 +32,14 @@ export default function Navbar () {
 				alt="webid_logo.01_branco_prancheta.png"
 				/>
 			</Link>
+
+			{size.width > 1002 && (
 			<ul>
+				<li>
+				<Link href="/">
+						Home
+					</Link>
+				</li>
 				<li>
 					<Link href="/auctions">
 						Auctions
@@ -35,7 +60,50 @@ export default function Navbar () {
 						About
 					</Link>
 				</li>
-			</ul>
+			</ul>)}
+			{/* SmallerScreens*/}
+			{menuOpen && size.width <= 1002 && (
+			<div className={styles.mobileMenu}>
+				<div className={styles.closeButton} onClick={closeMenu}>
+					{/*Close button (X)*/}
+					<span>&times;</span>
+				</div>
+				<ul className={`${styles.link_items}`}>
+				<li>
+				<Link href="/">
+						Home
+					</Link>
+				</li>
+				<li>
+					<Link href="/auctions">
+						Auctions
+					</Link>
+				</li>
+				<li>
+					<Link href="/create_new_auction">
+						Create New Auction
+					</Link>
+				</li>
+				<li>
+					<Link href="/contact">
+						Contact
+					</Link>
+				</li>
+				<li>
+					<Link href="/about">
+						About
+					</Link>
+				</li>
+          </ul>
+			</div>
+			)}
+			
+			{/*Renderiza o ícone de menu apenas em telas menores que 1002px */}
+			{!menuOpen && size.width <= 1002 && (
+				<div className={styles.menuIcon} onClick={toggleMenu}>
+          			<Image src={menuIcon} alt="Menu Icon" width={50} height={2} className="white-icon"/>
+				</div>
+			)}
 		</nav>
 	);
 }
