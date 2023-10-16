85e9aad12f66f06d6e40f44784c6b554e4a1b911
Iza Cabral <izabelecabral@hotmail.com>
2023-10-15 21:21:45 -0300

    contact page and fixing responsive issues

--- webid_front/pages/contact/index.js
+++ webid_front/pages/contact/index.js
@@ -1,15 +1,30 @@
 import Head from 'next/head';
 
+import styles from '../../styles/Contact.module.css';
+
 export default function Contact() {
 	return (
-		<div>
+		<div >
 			<Head>
 				<title>Contact</title>
 				<link rel="icon" href="/webid_logos/webid_favicon.ico" />
 			</Head>
 
-			<main>
-				<h1>Contact Page</h1>
+			<main className={styles.container}>
+				<div >
+					<h1>Contact</h1>
+					<p>
+						Imagine a bank-free loan, with lower fees, using tokenized assets like real estate as collateral. weBid embodies the full potential of blockchain technology: secure, cost-effective transactions with no unnecessary intermediaries.
+					</p>
+				</div>
+				<div>
+					<button className={styles.button}>
+						<a href="https://github.com/FernandaMatt/webid" target="_blank">GitHub</a>
+					</button>
+					<button className={styles.button}>
+						<a href="https://twitter.com/weBid_Finance" target="_blank">Twitter</a>
+					</button>
+				</div>
 			</main>
 		</div>
 	);
